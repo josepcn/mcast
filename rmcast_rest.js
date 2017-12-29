d907beebf0932b89b07e37c48bf46f6d997888c0
Josep Contreras <josep.contre@gmail.com>
2017-12-29 20:28:29 +0100

    javascript file

--- rmcast_rest.js
+++ rmcast_rest.js
@@ -1,9 +1,17 @@
 $.ajax({url: "https://g64vzck9m8.execute-api.eu-west-1.amazonaws.com/prod/list", 
 		success: function(result){
-	        //$("#div1").html(result);
-			$('#my-final-table').dynatable({
+			$('#my-final-table').dynatable(
+			{
 			  dataset: {
 			    records: result
+			  },
+			  features: {
+			    paginate: false,
+			    search: false,
+			    recordCount: false,
+			    perPageSelect: false
 			  }
-			});
+
+			}
+			);
     }});
